a157c97f3a1a495b697f7c4564851ead0c441af4
佐野啓吾 <sanokeigo@K5z-MacBook-Pro-336.local>
2026-02-24 23:34:47 +0900

    chore: cleanup stale files, fix debug_pin.h stub, add submodules

--- Source/stubs/clouds/drivers/debug/_pin.h
+++
@@ -1,7 +0,0 @@
-// Stub header replacing STM32 debug GPIO functionality. // Original: clouds/drivers/debug\_pin.h (hardware-specific) // This stub provides empty macros for desktop builds.
-
-#ifndef CLOUDS\_DRIVERS\_DEBUG\_PIN\_H\_ #define CLOUDS\_DRIVERS\_DEBUG\_PIN\_H\_
-
-#define TIC #define TOC
-
-#endif // CLOUDS\_DRIVERS\_DEBUG\_PIN\_H\_
---
+++ Source/stubs/clouds/drivers/debug_pin.h
@@ -0,0 +1,6 @@
+// Stub header replacing STM32 debug GPIO functionality.
+#ifndef CLOUDS_DRIVERS_DEBUG_PIN_H_
+#define CLOUDS_DRIVERS_DEBUG_PIN_H_
+#define TIC
+#define TOC
+#endif  // CLOUDS_DRIVERS_DEBUG_PIN_H_
